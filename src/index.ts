2eb4cb9ad427f89a7f7c753c92ca7f39ad27e54f
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-03-18 02:44:48 +0330

    Fix welcome, login and logout commands

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,47 @@
-import { bot } from "@app"
+import {bot, redisClient} from "@app"
+import {Middleware, session} from "telegraf"
+import {stage} from "@/commands/login.ts"
+import {Stage} from "telegraf/scenes"
+
+bot.start(async (ctx) => {
+    await ctx.replyWithMarkdownV2(`
+سلام ویستایی عزیز🧡
+من برات یه دوست مجازی‌ام که کمک کنم مطالعه‌تو منظم‌ و پیوسته کنی و توی یه رقابت سالم با بقیه ویستایاری‌ها، به جایی که دلت میخواد نزدیک‌تر بشی\\. داستانمونو شروع کنیم؟🧡🫱🏻‍🫲🏻
+
+برای ورود به حساب کاربری خود از دستور /login استفاده کنید\\.
+برای توقف هر دستور از دستور، \*خروج\* را ارسال کنید\\.
+`)
+})
 
 
-bot.start((ctx) => ctx.reply(`
-سلام!
-به بات ویستا خوش آمدید
-`))
+bot.command('login', async (ctx) => {
+    const user_exists = await redisClient.exists(ctx.chat.id.toString())
+
+    if (user_exists) {
+        await ctx.reply(`
+            شما قبلا وارد شدید!
+            برای خروج از حساب از دستور /logout استفاده کنید.
+        `)
+    }
+    else {
+        bot.use(session())
+        bot.use(stage.middleware())
+        Stage.action("redo", stage.middleware() as Middleware<any>)
+    }
+})
+
+bot.command('logout', async (ctx) => {
+    const user_exists = await redisClient.exists(ctx.chat.id.toString())
+
+    if (user_exists) {
+        await redisClient.del(ctx.chat.id.toString())
+        await ctx.reply(`
+        شما با موفیت از حساب خود خارج شدید.
+        برای ورود مجدد از دستور /login استفاده کنید.
+        `)
+    }
+    else await ctx.reply("شما در حساب خود وارد نشده اید. برای ورود به حساب از دستور /login استفاده نمایید.")
+})
 
 bot.command('where_am_i', (ctx) => ctx.reply('Hello'))
 bot.command('need_to_talk', (ctx) => ctx.reply('Hello'))

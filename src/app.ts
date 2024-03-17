68a42ac2f21c146d3f23fb6dc6a5cc7e8135a692
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-03-18 02:44:28 +0330

    Add, config and setup firebase and redis in project

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import dotenv from "dotenv"
 import {SocksProxyAgent} from "socks-proxy-agent"
-import {Telegraf} from "telegraf"
+import {Context, Telegraf} from "telegraf"
+import {Message, Update } from "telegraf/typings/core/types/typegram"
+import {CommandContextExtn} from "telegraf/typings/telegram-types"
+import {createClient} from "redis"
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
 
 dotenv.config()
 export const env = {
@@ -8,7 +13,15 @@ export const env = {
 } as {
     IS_DEV: string,
     BOT_TOKEN: string,
-    PROXY: string
+    PROXY: string,
+    REDIS_SERVER: string,
+
+    FIREBASE_apiKey: string,
+    FIREBASE_authDomain: string,
+    FIREBASE_projectId: string,
+    FIREBASE_storageBucket: string,
+    FIREBASE_messagingSenderId: string,
+    FIREBASE_appId: string,
 }
 
 export const isDev: boolean = env.IS_DEV === 'TRUE'
@@ -21,10 +34,39 @@ export const bot = new Telegraf(env.BOT_TOKEN, {
     }
 })
 
-export const cache: string[] = []
+const firebaseConfig = {
+    apiKey: env.FIREBASE_apiKey,
+    authDomain: env.FIREBASE_authDomain,
+    projectId: env.FIREBASE_projectId,
+    storageBucket: env.FIREBASE_storageBucket,
+    messagingSenderId: env.FIREBASE_messagingSenderId,
+    appId: env.FIREBASE_appId
+}
+
+// Initialize Firebase
+export const firebase = getFirestore(initializeApp(firebaseConfig))
+
+
+// Initialize redis client
+export const redisClient = createClient({
+    url: env.REDIS_SERVER
+})
+
+redisClient.on('error', err => console.error("Redis error: ", err))
+
+redisClient.connect()
+    .then(() => console.log(`Redis is running at ${env.REDIS_SERVER}`))
+    .catch((reason) => console.error("Redis error: ", reason))
+
+
+export type CommandContext =  Context<{
+    message: Update.New & Update.NonChannel & Message.TextMessage,
+    update_id: number
+}> & Omit<Context<Update>, keyof Context<any>> & CommandContextExtn
 
 bot.telegram.setMyCommands([
     {command: "login", description: "ورود به حساب"},
+    {command: "logout", description: "خروج از حساب"},
     {command: "where_am_i", description: "کجای مسیرم؟"},
     {command: "need_to_talk", description: " حالم بده میشه حرف بزنیم؟"},
     {command: "edit_point", description: "ویرایش هدف هفته"},

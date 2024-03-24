fab36d1410ca5b49beaac8640ab0d71f7b14dda7
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-03-24 04:56:08 +0330

    Fix find and insert goal logical bug and add update goal query into goal service

--- src/services/goal.service.ts
+++ src/services/goal.service.ts
@@ -3,9 +3,12 @@ import {Goal, goalConverter} from "@/schemas/Goal.ts"
 
 export const findGoalByUser = async (id: string) =>
     await firebase.collection("goals")
-        .where("user", "==", id)
-        .get()
+        .doc(id).get()
 
 export const insertGoal = async (goal: Goal) =>
     await firebase.collection("goals")
-    .doc().set(goalConverter.toFirestore(goal))
+    .doc(goal.getId()).set(goalConverter.toFirestore(goal))
+
+export const updateGoal = async (id: string, goal: { [key: string]: any }) =>
+    await firebase.collection("goals")
+    .doc(id).set(goal, {merge: true})

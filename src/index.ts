8662d014743553f7cee3c22e9d771b90a36dfff3
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-03-28 23:31:06 +0330

    Code daily schedule reminder and insert report and insert goal commands

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import {loginScene} from "@/scenes/login.ts"
 import {userConverter} from "@/schemas/User.ts"
 import {findById} from "@/services/user.service.ts"
 import {goalScene} from "@/scenes/goal.ts"
+import cron from "node-cron"
+import moment from "moment"
+import {iterateRedisKeys} from "@/functions.ts"
+import {findGoalByUser} from "@/services/goal.service.ts"
 
 bot.start(async (ctx) => {
     await ctx.replyWithMarkdownV2(`
@@ -52,11 +56,44 @@ bot.command('logout', async (ctx) => {
     else await ctx.reply("شما در حساب خود وارد نشده اید. برای ورود به حساب از دستور /login استفاده نمایید.")
 })
 
+bot.command('insert_report', async (ctx: CommandContext) => {
+    const user_cache = await redisClient.hGetAll(ctx.chat.id.toString())
+
+    const goal = await findGoalByUser(user_cache.id)
+
+    if (!goal.exists) await ctx.reply("شما هنوز هدفی برای این هفته ثبت نکردید. لطفا از طریق دستور /insert_goal هدف خود را ثبت کنید.")
+    else await ctx.scene.enter("goal", {is_report: true})
+})
+
+bot.command('insert_goal', async (ctx: CommandContext) => {
+    const user_cache = await redisClient.hGetAll(ctx.chat.id.toString())
+
+    const goal = await findGoalByUser(user_cache.id)
+
+    if (goal.exists) await ctx.reply("شما قبلا هدف خود را ثبت کرده اید. برای ویرایش آن از طریق دستور /edit_goal اقدام کنید.")
+    else await ctx.scene.enter("goal")
+})
+
+
 bot.command('where_am_i', (ctx) => ctx.reply('Hello'))
 bot.command('need_to_talk', (ctx) => ctx.reply('Hello'))
 bot.command('edit_point', async (ctx: CommandContext) => await ctx.scene.enter("goal", {is_update: true}))
 
 bot.launch()
+
+cron.schedule("0 8 * * *", async () => {
+    const today = moment().isoWeekday()
+
+    if (today !== 5 && today !== 6) {
+        await iterateRedisKeys(async (value: { id: string, username: string, chat_id: string }) => {
+            console.log(value)
+            await bot.telegram.sendMessage(value.chat_id.toString(), "سلام! صبح بخیر. لطفا گزارش روزانه خود را از طریق دستور /insert_report ثبت کنید.")
+        })
+    }
+}, {
+    scheduled: true,
+    timezone: "Asia/Tehran"
+})
 
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))

2fd3840ff36f2b7f5101b6a1e03f234a9b493174
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-07-20 18:58:07 +0330

    Remove unused files and code

--- src/schemas/Goal.ts
+++ src/schemas/Goal.ts
@@ -1,6 +1,3 @@
-import {v4 as uuidv4} from "uuid"
-import {DocumentData, QueryDocumentSnapshot, DocumentSnapshot} from "firebase-admin/firestore"
-
 export type Report = {
     test_count: number
     reading_time: number
@@ -14,48 +11,4 @@ export type Reports = {
     wednesday?: Report | null,
     thursday?: Report | null,
     friday?: Report | null
-}
-
-export class Goal implements DocumentData {
-    private readonly id: string
-    public test_count: number
-    public reading_time: number
-    public reports: Reports
-
-    constructor(id: string | null, test_count: number, reading_time: number, reports: Reports | null) {
-        this.id = id ?? uuidv4()
-        this.test_count = test_count
-        this.reading_time = reading_time
-        this.reports = reports ?? {
-            saturday: null,
-            sunday:  null,
-            monday: null,
-            tuesday: null,
-            wednesday: null,
-            thursday: null,
-            friday: null
-        }
-    }
-
-    getId() {
-        return this.id
-    }
-}
-
-export const goalConverter = {
-    toFirestore: (goal: Goal) => ({
-        test_count: goal.test_count,
-        reading_time: goal.reading_time,
-        reports: goal.reports,
-    }),
-    fromFirestore: (snapshot: QueryDocumentSnapshot | DocumentSnapshot): Goal => {
-        const data = snapshot.data()
-
-        return new Goal(
-            snapshot.id,
-            data.test_count,
-            data.reading_time,
-            data.reports,
-        )
-    }
 }

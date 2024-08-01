43b5d39ea065e3aa9aa6091b6c37f1a3f4bec456
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-08-01 21:56:41 +0330

    Fix sendMessage exception

--- src/schedules/insert_report.ts
+++ src/schedules/insert_report.ts
@@ -2,7 +2,12 @@ import {iterateRedisKeys} from "@/functions.ts"
 import {bot} from "@app"
 
 export const insert_report_schedules = async () => {
-    await iterateRedisKeys(async (value: { id: string, username: string, chat_id: string }) => {
-        await bot.telegram.sendMessage(value.chat_id.toString(), "سلام! شب بخیر. لطفا گزارش روزانه خود را از طریق دستور /insert_report ثبت کنید.")
+    await iterateRedisKeys(async (value: { id: string, chat_id: string }) => {
+        try {
+            await bot.telegram.sendMessage(value.chat_id.toString(), "سلام! شب بخیر. لطفا گزارش روزانه خود را از طریق دستور /insert_report ثبت کنید.")
+        }
+        catch (e) {
+            console.error(`Error while sending message to user ${value.id}. chat: ${value.chat_id.toString()}`)
+        }
     })
 }
